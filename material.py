cc80bdc17a5d3038a8faa399d353e03b41e40862
jonecherry <junchen.unif@qq.com>
2016-11-29 11:40:16 +0800

    新增永久素材

---
+++ material.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# filename: material.py
+import urllib2
+import json
+import poster.encode
+from poster.streaminghttp import register_openers
+import requests
+
+# from basic import Basic
+
+class Material(object):
+    #上传图文
+    def add_news(self, accessToken, news):
+        postUrl = "https://api.weixin.qq.com/cgi-bin/material/add_news?access_token=%s" % accessToken
+        urlResp = urllib2.urlopen(postUrl, news)
+        print urlResp.read()
+
+    # 上传
+    def uplaod(self, accessToken, filePath, mediaType):
+
+        openFile = open(filePath, "rb")
+        param = {'media': openFile}
+        postData, postHeaders = poster.encode.multipart_encode(param)
+
+        postUrl = "https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=%s&type=%s" % (accessToken, mediaType)
+        request = urllib2.Request(postUrl, postData, postHeaders)
+        urlResp = urllib2.urlopen(request)
+        print urlResp.read()
+
+    def get(self, accessToken, mediaId):
+        postUrl = "https://api.weixin.qq.com/cgi-bin/media/get?access_token=%s&media_id=%s" % (accessToken, mediaId)
+        urlResp = urllib2.urlopen(postUrl)
+
+        headers = urlResp.info().__dict__['headers']
+        if ('Content-Type: application/json\r\n' in headers) or ('Content-Type: text/plain\r\n' in headers):
+            jsonDict = json.loads(urlResp.read())
+            print jsonDict
+        else:
+            buffer = urlResp.read()  # 素材的二进制
+            mediaFile = file("test_media.jpg", "wb")
+            mediaFile.write(buffer)
+            print "get successful"
+
+    # 获取素材列表
+    def batch_get(self, accessToken, mediaType, offset=0, count=20):
+        postUrl = ("https://api.weixin.qq.com/cgi-bin/material"
+                   "/batchget_material?access_token=%s" % accessToken)
+        postData = ("{ \"type\": \"%s\", \"offset\": %d, \"count\": %d }"
+                    % (mediaType, offset, count))
+        urlResp = urllib2.urlopen(postUrl, postData)
+        print urlResp.read()
+
